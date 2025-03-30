1d194422d4c9a6b3c9c27fb5c0a1a352abc3765f
Nbvhoang08 <hoangnbv@hapiga.net>
2025-03-30 23:58:15 +0700

    DemoUI , IObsever

---
+++ Out to Sea Adventure/Assets/GAME/Scrip/HomeUI.cs
@@ -0,0 +1,49 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.Events;
+
+public class HomeUI : MonoBehaviour
+{
+    public RectTransform uiTop; 
+    public RectTransform uiBottom; 
+    public float moveDistance = 500f; 
+    public float duration = 0.5f; 
+    public static UnityAction StartGame;
+    private Vector2 _topStartPos;
+    private Vector2 _bottomStartPos;
+
+    void Awake()
+    {
+        _topStartPos = uiTop.anchoredPosition;
+        _bottomStartPos = uiBottom.anchoredPosition;
+        StartGame += OnClickButton;
+    }
+    void OnDestroy()
+    {
+        StartGame -= OnClickButton;
+    }
+    void OnEnable()
+    {
+        // Đặt UI ở ngoài màn hình
+        uiTop.anchoredPosition = _topStartPos + new Vector2(0, moveDistance);
+        uiBottom.anchoredPosition = _bottomStartPos - new Vector2(0, moveDistance);
+
+        // Hiệu ứng trượt vào
+        uiTop.DOAnchorPos(_topStartPos, duration).SetEase(Ease.OutBack);
+        uiBottom.DOAnchorPos(_bottomStartPos, duration).SetEase(Ease.OutBack);
+    }
+
+    public void OnClickButton()
+    {
+        uiTop.DOAnchorPos(_topStartPos + new Vector2(0, moveDistance), duration)
+             .SetEase(Ease.InBack);
+           
+
+        uiBottom.DOAnchorPos(_bottomStartPos - new Vector2(0, moveDistance), duration)
+                .SetEase(Ease.InBack)
+                .OnComplete(() => DOVirtual.DelayedCall(0.5f,() => gameObject.SetActive(false)));
+    }
+}
---
+++ Out to Sea Adventure/Assets/GAME/Scrip/UIButton.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using DG.Tweening;
+
+public class UIButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler,IPointerDownHandler, IPointerUpHandler
+{
+    private Button _button;
+    private Vector3 _originalScale;
+    private Color _originalColor;
+
+    public Color hoverColor = Color.cyan;
+    public float clickScaleFactor = 0.9f; 
+    public float scaleFactor = 1.1f; 
+    public float duration = 0.2f; 
+
+    void Start()
+    {
+        _button = GetComponent<Button>();
+        _originalScale = transform.localScale;
+        _originalColor = _button.image.color; 
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        transform.DOScale(_originalScale * scaleFactor, duration).SetEase(Ease.OutBack);
+        _button.image.DOColor(hoverColor, duration);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        transform.DOScale(_originalScale, duration).SetEase(Ease.InOutQuad);
+        _button.image.DOColor(_originalColor, duration);
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        transform.DOScale(_originalScale * clickScaleFactor, duration * 0.5f).SetEase(Ease.InOutQuad);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        transform.DOScale(_originalScale * scaleFactor, duration * 0.5f).SetEase(Ease.OutBack).OnComplete(() =>HomeUI.StartGame?.Invoke());
+    }
+}
